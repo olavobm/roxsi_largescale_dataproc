9dbc1e2934b03224140c424ee81507ecce0fb1e1
olavobm <olavobm@gmail.com>
2022-08-17 11:08:41 -0700

    Created function that outputs repository directory path.

--- repo_dirpath.m
+++ repo_dirpath.m
@@ -1,18 +1,17 @@
-function dirpath = repo_dirpath('')
-%%
+function dirpath = repo_dirpath()
+%% dirpath = repo_dirpath()
 %
-%
-%
+% REPO_DIRPATH.m is a function with no arguments
+% that returns the directory of where this function
+% is located. The idea is that this function is
+% in outermost directory of this repository.
 
-%%
+%
+full_filename = mfilename('fullpath');
 
 %
-if nargin==0
-    %
-    full_filename = mfilename('fullpath');
-    %
-    ind_sep = find(full_filename, filesep);
+ind_sep = strfind(full_filename, filesep);
 
-    %
-    dirpath = full_filename(1:(ind_sep(end)-1));
-end
+%
+dirpath = full_filename(1:(ind_sep(end)-1));
+

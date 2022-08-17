d46dd4823ef8116d9ae9d749c39def32969d3f6e
olavobm <olavobm@gmail.com>
2022-08-17 11:15:51 -0700

    Created function that outputs data parent directory.

---
+++ data_dirpath.m
@@ -0,0 +1,16 @@
+function dirpath = data_dirpath()
+%% dirpath = DATA_DIRPATH()
+%
+% DATA_DIRPATH.m is a function with no arguments
+% that returns the parent data directory.
+%
+% This can be set automatically (i.e. relative
+% to the repository directory) or can be edited
+% to point to a different location where the
+% dataset has been saved.
+
+% Setting it relative to the repository direction
+file_fullname = mfilename('fullpath');
+
+%
+dirpath = fileparts(fileparts(file_fullname));

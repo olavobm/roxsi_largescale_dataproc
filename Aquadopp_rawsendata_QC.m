a80839764ab82a487531242fe46534001280cb85
olavobm <olavobm@gmail.com>
2022-08-19 09:33:34 -0700

    Created QC file that looks at variables in Aquadopp *.sen file

---
+++ Aquadopp_rawsendata_QC.m
@@ -0,0 +1,9 @@
+%% Plot initial QC plots for Aquadopp data:
+%   1) timeseries of pressure, heading, pitch, and roll
+%   2) timeseries zoomed around recovery time to get the
+%      recovery time and look at pressure when instrument
+%      was OUT of the water.
+
+clear
+close all
+

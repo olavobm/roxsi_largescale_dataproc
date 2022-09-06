3c49607b4cb550ef9c768255c35c367c116131b5
olavobm <olavobm@gmail.com>
2022-09-06 14:41:22 -0700

    First commit of this function (that is mostly done already).

---
+++ RBR_extract_loginfo.m
@@ -0,0 +1,141 @@
+function loginfo = RBR_extract_loginfo(log_filename, day_info, SN)
+%% loginfo = RBR_EXTRACT_LOGINFO(log_filename, day_info, SN)
+%
+%   inputs
+%       - log_filename: file name (with extension).
+%       - day_info: in the format yyyy-mm-dd.
+%       - SN: serial number of the sensor of interest
+%
+%   outputs
+%       - loginfo:
+%
+%
+%
+%
+%
+%
+%
+
+%% 
+
+%
+fid = fopen(log_filename);
+
+
+%
+lfound_SN = false;
+
+%
+loginfo = [];
+
+tic
+%
+% while 1 && (~lfound_SN)    % this assumes that after finding one instance
+                             % of instrument SN and reaching the following instrument with a different
+                             % serial number, there is no more informatio about the first instrument
+
+% %      %
+% % disp(' ')
+% % disp(' ')
+
+% Read the full file
+while 1
+    
+    % Read new line and terminate loop at the end of file
+    tline = fgetl(fid);
+    if ~ischar(tline), break, end
+
+    
+    % Only look for information if it's on the day
+    % of the programming
+    if strncmp(tline, day_info, length(day_info))
+
+        %
+        if contains(tline, 'RESPONSE: id model = RBRsolo,') && contains(tline, SN)
+
+            % Print message to the screen that log information
+            % has been found for the specific RBR instrument
+            disp(['--- log info from RBR SN ' SN ' has been found ---'])
+
+            % If the instrument SN has NOT been found yet
+            if ~lfound_SN
+                %
+                lfound_SN = true;
+
+            % If the instrument SN has already been found, then there 
+            % is a second instance with information on instrument SN.
+            else
+                % Throw a warning and break the loop/end the function
+                warning(['A second instance of RBR SN ' SN ' was found!!!'])
+
+                %
+                break
+            end
+
+% %             lfound_SN = true;
+% %             keyboard
+
+            %
+            while lfound_SN
+
+
+                %
+                if contains(tline, 'RESPONSE: now =') || ...
+                   contains(tline, 'Apparent time difference between logger and host:')
+
+% % %                         % Output line information
+% % %                         tline
+% %                         keyboard
+
+                    % First iteration/line
+                    if isempty(loginfo)
+                        %
+                        loginfo = string(tline);
+                    % Append other lines in a string array
+                    else
+                        loginfo = [loginfo; string(tline)];
+                    end
+                    
+                end
+                
+                %
+                if contains(tline, 'RESPONSE: id model = RBRsolo,')
+                    if ~contains(tline, SN)
+%                         %
+%                         lfound_SN = false;
+                        %
+                        disp(['--- Done with getting log info from RBR SN ' SN ' ---'])
+
+                        %
+                        break
+                    end
+                end
+                
+                % Read the next line for the next iteration
+                % of the while loop
+                tline = fgetl(fid);
+                %
+                % (note there is a potential, but unlikely problem
+                % here). When . 
+
+                % If the instrument SN is the last one in the
+                % file, then the inner loop needs to be told
+                % that then end of the file has been reached
+                if ~ischar(tline), break, end
+            end
+
+        end
+    end
+
+
+end
+%
+fclose(fid);
+toc
+
+%
+if isempty(loginfo)
+    %
+    warning(['--- NO log on RBR SN ' SN ' was found!!! ---'])
+end
+

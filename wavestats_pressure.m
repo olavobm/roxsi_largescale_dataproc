d959b5fc028bcb35a165206c4a158ad92612e481
olavobm <olavobm@gmail.com>
2022-09-18 17:56:46 -0700

    Just started function to compute wave statistics from pressure time series.

---
+++ wavestats_pressure.m
@@ -0,0 +1,13 @@
+function statsout = wavestats_pressure(pressuredata)
+% statsout = WAVESTATS_PRESSURE(pressuredata)
+%
+%   inputs
+%       - pressuredata: timegridded pressure data (vector)????
+%
+%   outputs
+%       -
+%
+%
+%
+%
+%

aedb32463279d9679f1c040d749ac1bd994532d8
olavobm <olavobm@gmail.com>
2022-09-20 19:17:23 -0700

    adapted most of the code, but still need to take care of outputs and probably make arrays of consistent size.

--- bulkstats_from_wave_dirspectrum.m
+++ bulkstats_from_wave_dirspectrum.m
@@ -1,11 +1,12 @@
-function wvspec = bulkstats_from_wave_dirspectrum(wvspec, dirvec, dspec)
-%% bulk = BULKSTATS_FROM_WAVE_DIRSPECTRUM(wvspec, dirvec, dspec)
+function dir = bulkstats_from_wave_dirspectrum(freq, dirvec, Dir_f_fcn, wvdirspec, wvspec)
+%% bulk = BULKSTATS_FROM_WAVE_DIRSPECTRUM(freq, dirvec, Dir_f_fcn, wvdirspec, wvspec)
 %
 %   inputs
-%       - dspec:
-%       - theta: the direction vector associated with the directional
-%                spectrum. In degrees, clockwise from true north, from
-%                zero towards 360).
+%       - freq: in Hz.
+%       - dirvec:
+%       - Dir_f_fcn:
+%       - wvdirspec:
+%       - wvspec:
 %
 %   outputs
 %       - bulk: 
@@ -24,8 +25,6 @@ function wvspec = bulkstats_from_wave_dirspectrum(wvspec, dirvec, dspec)
 % Olavo Badaro Marques.
 
 % Computed statistiscal quantities are:
-%   - preak frequency:
-%   - signifcant wave height:
 %
 %   - dir_mean_f:
 %   - spread_mean_f:
@@ -39,62 +38,77 @@ function wvspec = bulkstats_from_wave_dirspectrum(wvspec, dirvec, dspec)
 %%
 
 % Compute direction in radians and turn into column
-dirtheta = deg2rad(dirvec);
-dirtheta = dirtheta(:);
+dirinradians = deg2rad(dirvec);
+dirinradians = dirinradians(:);
 
-% % %
+% % % May want to include this later
 % % guess = deg2rad(guess);
 
+% % % I should be able to compute this from
+% % % integrating the directional spectrum,
+% % % and all methods give the same result, right?!
+% % % If so, then I don't need to input wvspec.
+% % % 
+% % waveSpec = wvspec.S_f
+
+
+%%
+
+% % %
+% % if size(wvdirspec, 3)~=1
+% %     wvdirspec
+% % end
+
+%
+Nfrequency = length(freq);
+Ntime = size(wvdirspec, 3);
 
 
 %%
 
 %
-Ntime = length(wvspec.dtime);
-Nfrequency = length(wvspec.f);
+prealloc_aux = NaN(1, Ntime);
+%
+dir_peak = prealloc_aux;
+spread_peak = prealloc_aux;
+%
+ind_f_pk = prealloc_aux;
+%
+theta0 = NaN(Nfrequency, Ntime);
+sigma0 = NaN(Nfrequency, Ntime);
 
 %
-vec_like_dtime = NaN(size(wvspec.dtime));
-%
-dir_peak = vec_like_dtime;
-spread_peak = vec_like_dtime;
-%
-theta0 = NaN(Ntime, Nfrequency);
-sigma0 = NaN(Ntime, Nfrequency);
-%
-ind_f_pk = vec_like_dtime;
+disp(['-------------- Total number of time stamps is = ' num2str(Ntime) ' --------------'])
 
-%
-disp(['Total number of timestamps is = ' num2str(length(wvspec.dtime))])
 
-% Loop over time (Skip the last one because I need to fix the code
-% that computes spectrum)
+%% Compute frequency spectra of the mean direction and mean direction spread
+
+% Loop over time
+%
+% (Skip the last one because I need to fix the code that computes spectrum)
 tic
-for timestep = 1:(length(wvspec.dtime)-1)
-
-    % the index of the peak frequency
-    [~, ind_f_pk(timestep)] = max(wvspec.S_f(:, timestep));
+for i1 = 1:(Ntime-1)
 
     % Loop over frequency
-    for ii = 1 : length(wvspec.f)
+    for i2 = 1 : Nfrequency
     
         % Create a circular version of the directional spread 
-        D = dspec.D_f_theta(ii, :, timestep);
+        D = Dir_f_fcn(i2, :, i1);
         D = D(:);
         %
         [~, ind_max] = max(D);
-        guess = dirtheta(ind_max);
+        guess = dirinradians(ind_max);
         
         % use fzero to solve for theta0, per timestep, per frequency.
-        theta0(timestep, ii) = fzero(@(theta0) trapz(dirtheta, sin(dirtheta-theta0).*D), guess);
-        sigma0(timestep, ii) = rad2deg(sqrt(4.*trapz(dirtheta, (sin((dirtheta-theta0(timestep, ii))./2).^2).*D)));
+        theta0(i1, i2) = fzero(@(theta0) trapz(dirinradians, sin(dirinradians-theta0).*D), guess);
+        sigma0(i1, i2) = rad2deg(sqrt(4.*trapz(dirinradians, (sin((dirinradians-theta0(i1, i2))./2).^2).*D)));
     end
 
     % Print progress message
-    fractional_progress = round(100*timestep/length(wvspec.dtime));
+    fractional_progress = round(100*i1/Ntime);
     %
     if fractional_progress~=0 && mod(fractional_progress, 10)==0
-        disp(['Done with timestamp ' num2str(timestep) ' out of ' num2str(length(wvspec.dtime))])
+        disp(['Done with timestamp ' num2str(i1) ' out of ' num2str(Ntime)])
         toc
     end
 end
@@ -103,20 +117,34 @@ end
 %
 theta0 = mod(theta0, 2*pi);
 
-%%
+
+%% Compute mean direction, mean directional spread, peak direction
+% and directional spread of the peak direction
+%
 % this weight is for the directional averaging because it is a circular
-% variable, the normal energy weighting has to be modified. 
+% variable, the normal energy weighting has to be modified.
+%
 % Lines have been added here to remove the impact when the spread
 % function is uniform
-weight = (wvspec.f(2) - wvspec.f(1)) .* wvspec.S_f ./ trapz(wvspec.f, wvspec.S_f);
+
+%
+weight = (freq(2) - freq(1)) .* wvspec ./ trapz(freq, wvspec);
 weight(isnan(theta0')) = 0;
 
 % THIS NEEDS TO BE BROKEN DOWN FOR CLARITY!!!!
-dir_mean = mod(atan2d(sum(weight.*sind(rad2deg(theta0(:,:)'))), sum(weight.*cosd(rad2deg(theta0(:,:)')))), 360);
-spread_mean = trapz(wvspec.f, wvspec.S_f.*sigma0') ./ trapz(wvspec.f, wvspec.S_f);
+%
+dir_mean = mod(atan2d(sum(weight.*sind(rad2deg(theta0(:, :)'))), ...
+                      sum(weight.*cosd(rad2deg(theta0(:, :)')))), ...
+               360);
+%
+spread_mean = trapz(freq, wvspec.*sigma0') ./ trapz(freq, wvspec);
 
 % these lines pick out the direction and spread for the peak frequency
-for ii_dt = 1 : (length(wvspec.dtime) - 1)
+for ii_dt = 1 : (Ntime - 1)
+
+    % the index of the peak frequency
+    [~, ind_f_pk(ii_dt)] = max(wvspec(:, ii_dt));
+
     %
     dir_peak(ii_dt) = rad2deg(theta0(ii_dt, ind_f_pk(ii_dt)));
     spread_peak(ii_dt) = sigma0(ii_dt, ind_f_pk(ii_dt));
@@ -125,11 +153,14 @@ end
 
 %% Assign bulk statistics to output variable
 
-% Bulk statistic from directional wave spectrum
+%
 dspec.dir_mean_f = rad2deg(theta0)';
 dspec.spread_mean_f = sigma0';
+
+%
 dspec.dir_mean = dir_mean(:);
 dspec.spread_mean = spread_mean(:);
+%
 dspec.dir_peak = dir_peak(:);
 dspec.spread_peak = spread_peak(:);
 

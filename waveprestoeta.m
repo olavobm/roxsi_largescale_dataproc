9909666243582490c1fc6c4de28ab0f40e5dc7f6
olavobm <olavobm@gmail.com>
2022-09-30 16:22:09 -0700

    Created function to do the low-level calculation of conversion from pressure to vertical surface displacement.

---
+++ waveprestoeta.m
@@ -0,0 +1,18 @@
+function waveeta = waveprestoeta(wavep, hi, dtsampling, freqcutoff)
+%% waveeta = WAVEPRESTOETA(wavep, hi, dtsampling, freqcutoff)
+%
+%   inputs
+%       -
+%       -
+%       -
+%       -
+%
+%   outputs
+%       -
+%
+%
+%
+%
+%
+%
+%

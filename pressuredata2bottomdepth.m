8e9ce9bf39e47defc4ecb46f3cddd945114400fd
olavobm <olavobm@gmail.com>
2022-09-30 17:26:37 -0700

    Created empty function.

---
+++ pressuredata2bottomdepth.m
@@ -0,0 +1,14 @@
+function bottomdepth = pressuredata2bottomdepth(pressure, hi, H, dtsampling, freqcutoff)
+%% bottomdepth = PRESSUREDATA2BOTTOMDEPTH(pressure, hi, H, dtsampling, freqcutoff)
+%
+%
+%
+%
+%
+%
+%
+%
+%
+%
+%
+%

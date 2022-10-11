aa77a0b757aca11722af2c01c6c4be42b15f827d
olavobm <olavobm@gmail.com>
2022-10-11 10:51:28 -0700

    Initialized empty function (will largely be a copy of another function).

---
+++ spectra_scalar.m
@@ -0,0 +1,24 @@
+function [Sxx, varinbands] = spectra_scalar(timedata, x, dtdata, timespec, windowfft, freqbands)
+%% [Sxx, varinbands] = SPECTRA_SCALAR(timedata, x, dtdata, timespec, windowfft)
+%
+%   inputs
+%       -
+%       -
+%       -
+%       -
+%       -
+%       -
+%
+%   outputs
+%       -
+%       -
+%
+%
+%
+%
+%
+%
+
+
+%%
+

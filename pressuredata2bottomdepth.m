3380d00b0f0a85a877f4b32f284201a182a3dacc
olavobm <olavobm@gmail.com>
2022-10-12 14:58:31 -0700

    Added header.

--- pressuredata2bottomdepth.m
+++ pressuredata2bottomdepth.m
@@ -1,5 +1,19 @@
-function bottomdepth = pressuredata2bottomdepth(pressure, hi, H, dtsampling, freqcutoff)
-%% bottomdepth = PRESSUREDATA2BOTTOMDEPTH(pressure, hi, H, dtsampling, freqcutoff)
+function bottomdepth = wavepdata2eta(pressure, hi, H, dtsampling, freqcutoff, dt)
+%% bottomdepth = PRESSUREDATA2BOTTOMDEPTH(pressure, hi, H, dtsampling, freqcutoff, dtlowfreq)
+%
+%   inputs
+%       - wavep: wave pressure anomaly (in dbar).
+%       - hi: height above the bottom (in meters) of the
+%             instrument/pressure data.
+%       - H: bottom depth (in meters).
+%       - dtsampling: sampling period (in seconds).
+%       - freqcutoff: high-frequency cut off (in Hertz).
+%       - dtlowfreq: time scale (in seconds) of the low-frequency
+%                    signal (which will be assumed to be hydrostatic).
+%
+%   outputs
+%       - waveeta: surface displacement (in meters) relative
+%                  to the mean position.
 %
 %
 %
@@ -9,6 +23,7 @@ function bottomdepth = pressuredata2bottomdepth(pressure, hi, H, dtsampling, fre
 %
 %
 %
-%
-%
-%
+
+
+%%
+

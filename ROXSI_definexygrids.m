b9ea2d927a34fd6437174ba499c529211df874e7
olavobm <olavobm@gmail.com>
2022-10-13 13:41:27 -0700

    harmless change on the last decimal points.

--- ROXSI_definexygrids.m
+++ ROXSI_definexygrids.m
@@ -10,16 +10,19 @@ clear
 % The angle is the arc (in clockwise degrees) from the
 % geographical north to the offshore direction.
 
+% Asilomar
 %
-roxsigrid.Asilomar.lonref = -121 -(56/60) -(25.19048/3600);
-roxsigrid.Asilomar.latref = 36 +(37/60) +(26.5187/3600);
+roxsigrid.Asilomar.latref = 36 +  (37/60) + (26.5187/3600);
+roxsigrid.Asilomar.lonref = -121 -(56/60) - (25.19048/3600);
 %
 % roxsigrid.Asilomar.angleref = -23;    % angle of rotation (270 - 293)
 roxsigrid.Asilomar.angleref = 293;
 
+% China Rock
 %
-roxsigrid.ChinaRock.lonref = -121 -(57/60) -(33.813386/3600);
-roxsigrid.ChinaRock.latref = 36 +(36/60) +(15.892765/3600);
+roxsigrid.ChinaRock.latref = 36 +  (36/60) + (15.8927651999/3600);
+roxsigrid.ChinaRock.lonref = -121 -(57/60) - (33.8133851999/3600);
+%
 % roxsigrid.ChinaRock.angleref = -15;    % angle of rotation (270 - 285)
 roxsigrid.ChinaRock.angleref = 285;
 

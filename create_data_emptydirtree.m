2da4016b936349d1cbd191701509dc48584b2eb2
olavobm <olavobm@gmail.com>
2022-10-17 14:52:33 -0700

    started script to initialize directory tree,

---
+++ create_data_emptydirtree.m
@@ -0,0 +1,19 @@
+%% Create directory tree with no files for temporary data.
+% It starts by removing an existing dir tree. !!!BE CAREFUL WITH THIS!!!
+
+clear
+close all
+
+
+%%
+
+%
+dir_parent_location = '';
+
+%
+dir_parent_name = '';
+
+
+%% Remove (or rename) an existing dir tree that
+% matches with what this script creates
+

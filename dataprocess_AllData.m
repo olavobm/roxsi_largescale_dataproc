27455d4ff81b80400e6da00ebe3d9f99139dbf5a
olavobm <olavobm@gmail.com>
2022-10-17 14:04:24 -0700

    small changes

--- dataprocess_AllData.m
+++ dataprocess_AllData.m
@@ -7,14 +7,26 @@ close all
 
 
 
-%% Process Smart Moorings
-
-%
-run('Spotters_proc_lvl_1.m')
-
-
-%% Process Spotters
+%% Process  Smart Moorings
 
 %
 run('SpottersSmart_proc_lvl_1.m')
 run('SpottersSmart_timegrid_pressure.m')
+%
+run('SpottersSmart_proc_lvl_2.m')
+
+
+
+%% Process Spotters (note that the above is currently
+% required for L2 processing of smart mooring data)
+
+%
+run('Spotters_proc_lvl_1.m')
+run('Spotters_proc_lvl_2.m')
+
+
+%% Aquadopps
+
+
+
+%% Signature1000

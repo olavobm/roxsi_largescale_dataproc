1dad0667f454b65b8efcc370467617258b313ace
olavobm <olavobm@gmail.com>
2022-10-18 09:14:33 -0700

    Added frequency band in the input.

--- bulkstats_from_wave_spectrum.m
+++ bulkstats_from_wave_spectrum.m
@@ -1,9 +1,12 @@
-function [freq_peak, freq_mean, Hsig] = bulkstats_from_wave_spectrum(freq, wvspec)
-%%  [freq_peak, freq_mean, Hsig] = BULKSTATS_FROM_WAVE_SPECTRUM(freq, wvspec)
+function [freq_peak, freq_mean, Hsig] = bulkstats_from_wave_spectrum(freq, wvspec, freqband)
+%%  [freq_peak, freq_mean, Hsig] = BULKSTATS_FROM_WAVE_SPECTRUM(freq, wvspec, freqband)
 %
 %   inputs
 %       - freq: in Hz.
 %       - wvspec: wave spectrum. If a matrix, 
+%       - freqband: 1x2 vector with frequency band (in Hz)
+%                   where mean frequency and significant wave height
+%                   will be computed.
 %
 %   outputs
 %       - freq_peak:
@@ -25,13 +28,28 @@ if isvector(wvspec)
     wvspec = wvspec(:);
 end
 
+%
+if ~exist('freqband', 'var')
+    freqband = [0, +Inf];
+end
+
+
+%%
+
+%
+linfreqband = (freq >= freqband(1)) & (freq <= freqband(2));
+
+% Subset input within frequency band
+freq = freq(linfreqband);
+wvspec = wvspec(linfreqband, :);
+
 
 %%
 
 % Find index of the peak for each column in the wave height spectrum
 [~, ind] = max(wvspec, [], 1, 'includenan');
 
-% Frequency of the peak in the spectrum
+% Frequency of the peak in the spectrum/spectra
 freq_peak = freq(ind);
 
 % Mean period and frequency

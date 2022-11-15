dc24b05ea52dd15e81667331409ca9809f022362
olavobm <olavobm@gmail.com>
2022-11-15 11:48:27 -0800

    Initialized script to merge buoy and pressure L1 data for smart moorings.

---
+++ SmartMooring_merge_L1.m
@@ -0,0 +1,19 @@
+function mergedL1 = SmartMooring_merge_L1(dirbuoy, dirpressure, SN)
+%% mergedL1 = SmartMooring_merge_L1(dirbuoy, dirpressure, SN)
+%
+%
+%
+%
+%
+%
+%
+%
+%
+
+
+%%
+
+load()
+
+
+

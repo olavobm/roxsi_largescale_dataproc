8340f0d2a7665471ddfba7252310fed4d8605465
olavobm <olavobm@gmail.com>
2023-02-07 12:18:37 -0800

    Added (x, y) references for local grids at Garrapata and Hopkins.

--- ROXSI_definexygrids.m
+++ ROXSI_definexygrids.m
@@ -26,6 +26,20 @@ roxsigrid.ChinaRock.lonref = -121 -(57/60) - (33.8133851999/3600);
 % roxsigrid.ChinaRock.angleref = -15;    % angle of rotation (270 - 285)
 roxsigrid.ChinaRock.angleref = 285;
 
+% ----------------------------------
+% Garrapata -- NEED TO GET A CONFIRMATION ON THESE!!!
+%
+roxsigrid.Garrapata.latref = 36 +  (28/60) + (05.53/3600);
+roxsigrid.Garrapata.lonref = -121 -(56/60) - (04.20/3600);
+%
+roxsigrid.Garrapata.angleref = 270-20;
+
+% Hopkins
+%
+roxsigrid.Garrapata.latref = 36 +  (37/60) + (18.82/3600);
+roxsigrid.Garrapata.lonref = -121 -(54/60) - (14.34/3600);
+%
+roxsigrid.Garrapata.angleref = +20;
 
 
 %% Save roxsiGrid structure in the same folder as this script
@@ -36,3 +50,4 @@ indslash = strfind(fullnamepath, '/');
 
 %
 save(fullfile(fullnamepath(1:indslash(end)), 'ROXSI_xygrids.mat'), 'roxsigrid')
+

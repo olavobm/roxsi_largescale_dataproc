4e27997c04f0031e3597ec3f501b4fb1bf54b284
olavobm <olavobm@gmail.com>
2023-02-07 13:19:41 -0800

    small edit to comments.

--- ROXSI_definexygrids.m
+++ ROXSI_definexygrids.m
@@ -10,6 +10,7 @@ clear
 % The angle is the arc (in clockwise degrees) from the
 % geographical north to the offshore direction.
 
+% ------------
 % Asilomar
 %
 roxsigrid.Asilomar.latref = 36 +  (37/60) + (26.5187/3600);
@@ -18,6 +19,7 @@ roxsigrid.Asilomar.lonref = -121 -(56/60) - (25.19048/3600);
 % roxsigrid.Asilomar.angleref = -23;    % angle of rotation (270 - 293)
 roxsigrid.Asilomar.angleref = 293;
 
+% ------------
 % China Rock
 %
 roxsigrid.ChinaRock.latref = 36 +  (36/60) + (15.8927651999/3600);
@@ -27,13 +29,16 @@ roxsigrid.ChinaRock.lonref = -121 -(57/60) - (33.8133851999/3600);
 roxsigrid.ChinaRock.angleref = 285;
 
 % ----------------------------------
-% Garrapata -- NEED TO GET A CONFIRMATION ON THESE!!!
+% NEED TO GET A CONFIRMATION ON THESE!!!
+% ------------
+% Garrapata
 %
 roxsigrid.Garrapata.latref = 36 +  (28/60) + (05.53/3600);
 roxsigrid.Garrapata.lonref = -121 -(56/60) - (04.20/3600);
 %
 roxsigrid.Garrapata.angleref = 270-20;
 
+% ------------
 % Hopkins
 %
 roxsigrid.Garrapata.latref = 36 +  (37/60) + (18.82/3600);
